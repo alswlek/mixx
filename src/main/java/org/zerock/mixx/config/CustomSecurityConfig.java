ec7b022041ab761bab9ffc92172346953468af0d
smlee <dltnals0109@gmail.com>
2023-09-08 09:11:41 +0900

    login.html 로그아웃 맵핑에러 수정

--- src/main/java/org/zerock/mixx/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/mixx/config/CustomSecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -12,30 +13,50 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.zerock.mixx.security.CustomUserDetailsService;
 import org.zerock.mixx.security.handler.Custom403Handler;
-
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true) //boardcontrlloer -> register 에 @prepost처리
+@EnableGlobalMethodSecurity(prePostEnabled = true) //boardcontrlloer -> register 에 @prepost처리(사전사후권한체크)
+
 public class CustomSecurityConfig {
-//customuserdetailservice 가 정상적으로 동작하려면 bean 지정하고 customuserdetailservice에 주입
+    //customuserdetailservice 가 정상적으로 동작하려면 customuserdetailservice에 주입
+    private final CustomUserDetailsService userDetailsService;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    //인증처리를 위한 userdetailservice
-//    filterchain 적용하면 /board/list 에 접근할수 있게
+    //인증처리를 -> userdetailservice
+// filterchain 적용하면 /board/list 에 접근할수 있게
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         log.info("------------configure-------------------");
-        http.formLogin().loginPage("/member/login"); // board의register 가면 /member/login 이동 -> post 방식으로 처리p699
-        http.formLogin().defaultSuccessUrl("/index.html"); // 로그인 성공 시 이동할 URL
-        http.formLogin().failureUrl("/member/login?error"); // 페이지 만들어야 함
-        http.csrf().disable(); //csrf 비활성화 -> username,password만으로 로그인가능
+        http
+                .authorizeRequests()
+                    .antMatchers("/index.html").permitAll() // 로그인 없이 접근 가능한 페이지 설정
+// .antMatchers("/board/**").authenticated() //로그인하면 게시판볼수있게
+                    .anyRequest().permitAll()
+                    .and()
 
+                .formLogin()
+                    .loginPage("/member/login") // 로그인 페이지 URL 설정
+                    .defaultSuccessUrl("/index.html") // 로그인 성공 시 이동할 URL
+                    .failureUrl("/member/login?error") //로그인 실패 후 이동할 URL 설정
+                    .and()
+
+                .logout()
+                    .permitAll()
+                    .logoutUrl("/member/logout") // 로그아웃 URL 설정
+//                    .logoutSuccessUrl("/index.html") // 로그아웃 성공 시 리다이렉트 주소
+//                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/login?logout")) // 로그아웃 URL 변경
+
+                .invalidateHttpSession(true) // 세션 날리기
+                .deleteCookies("JSESSIONID"); //쿠키 삭제
+
+        http.csrf().disable(); //csrf 비활성화 -> username,password만으로 로그인가능
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler()); //403
         return http.build();
     }
@@ -44,11 +65,9 @@ public class CustomSecurityConfig {
         return new Custom403Handler();
     }
 
-    //    정적자원처리 css,js 파일 필터 제외
+    // 정적자원처리 css,js 파일 필터 제외
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-
-        log.info("------------web configure-------------------");
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
